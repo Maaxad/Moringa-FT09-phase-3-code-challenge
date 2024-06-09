c02cef54f4e139b3792d8b870ed234d65f95fb40
mahad <muhumedmohamed836@gmail.com>
2024-06-09 20:28:02 +0300

    done

--- models/article.py
+++ models/article.py
@@ -1,3 +1,5 @@
+from database.connection import get_db_connection
+
 class Article:
     def __init__(self, id, title, content, author_id, magazine_id):
         self.id = id
@@ -8,3 +10,72 @@ class Article:
 
     def __repr__(self):
         return f'<Article {self.title}>'
+
+    @property
+    def title(self):
+        return self._title
+    
+    @title.setter
+    def title(self, value):
+        if hasattr(self, '_title'):
+            raise AttributeError("Cannot change title after it has been set")
+        if not isinstance(value, str):
+            raise TypeError("Title must be a string")
+        if not (5 <= len(value) <= 50):
+            raise ValueError("Title must be between 5 and 50 characters")
+        self._title = value
+
+    def author(self):
+        return get_author(self.author_id)
+
+    def magazine(self):
+        return get_magazine(self.magazine_id)
+
+class Author:
+    def __init__(self, id, name, bio):
+        self.id = id
+        self.name = name
+        self.bio = bio
+
+    def __repr__(self):
+        return f'<Author {self.name}>'
+
+class Magazine:
+    def __init__(self, id, name, editor):
+        self.id = id
+        self.name = name
+        self.editor = editor
+
+    def __repr__(self):
+        return f'<Magazine {self.name}>'
+
+def get_author(author_id, conn):
+    cursor = conn.cursor()
+    sql = """
+        SELECT *
+        FROM authors
+        WHERE id = ?
+    """
+    cursor.execute(sql, (author_id,))
+    author_data = cursor.fetchone()
+
+    if author_data:
+        return Author(*author_data)
+    else:
+        return None
+
+def get_magazine(magazine_id, conn):
+    cursor = conn.cursor()
+    sql = """
+        SELECT *
+        FROM magazines
+        WHERE id = ?
+    """
+    cursor.execute(sql, (magazine_id,))
+    magazine_data = cursor.fetchone()
+
+    if magazine_data:
+        return Magazine(*magazine_data)
+    else:
+        return None
+
--- models/author.py
+++ models/author.py
@@ -1,7 +1,69 @@
+from database.connection import get_db_connection
+from models.article import Article
+from models.magazine import Magazine
+
 class Author:
     def __init__(self, id, name):
-        self.id = id
+        self._id = id
         self.name = name
 
+    @property
+    def id(self):
+        return self._id
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, name):
+        if not isinstance(name, str):
+            raise TypeError("name must be of type str")
+        if len(name) == 0:
+            raise ValueError("name must be longer than 0 characters")
+        if hasattr(self, '_name'):
+            raise AttributeError("name cannot be changed once set")
+        self._name = name
+
+    def _fetch_articles(self):
+        conn = get_db_connection()
+        cursor = conn.cursor()
+        cursor.execute('''
+            SELECT id, title, content, author_id, magazine_id
+            FROM articles
+            WHERE author_id = ?
+        ''', (self.id,))
+        article_info = cursor.fetchall()
+        conn.close()
+        return [Article(*article) for article in article_info]
+
+    @property
+    def articles(self):
+        if not hasattr(self, '_articles'):
+            self._articles = self._fetch_articles()
+        return self._articles
+
+    def _fetch_magazines(self):
+        conn = get_db_connection()
+        cursor = conn.cursor()
+        cursor.execute('''
+            SELECT DISTINCT m.id, m.name, m.category
+            FROM magazines m
+            JOIN articles a ON m.id = a.magazine_id
+            WHERE a.author_id = ?
+        ''', (self.id,))
+        magazine_info = cursor.fetchall()
+        conn.close()
+        return [Magazine(*magazine) for magazine in magazine_info]
+
+    @property
+    def magazines(self):
+        if not hasattr(self, '_magazines'):
+            self._magazines = self._fetch_magazines()
+        return self._magazines
+
     def __repr__(self):
-        return f'<Author {self.name}>'
+        article_titles = ", ".join(article.title for article in self.articles) if self.articles else "None"
+        magazine_titles = ", ".join(magazine.name for magazine in self.magazines) if self.magazines else "None"
+        return f'<Author: {self.name} | id: {self.id} | MAGAZINES: {magazine_titles} | ARTICLES: {article_titles}>'
+
